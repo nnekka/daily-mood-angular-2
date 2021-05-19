7efb2632a05e31d1da11023152bd47d643a87b2c
Nneka <nnekka@yandex.ru>
2021-05-19 10:13:35 +0300

    Calendar-frontend: добавила небольшую валидацию для дней календаря и немного рефакторинга

--- client/src/app/components/calendar/calendar.component.ts
+++ client/src/app/components/calendar/calendar.component.ts
@@ -18,7 +18,7 @@ export class CalendarComponent implements OnInit {
 
 
   days = Array(32).fill(0).map((p, i) => i)
-  months: string[] = ['', 'Янв', 'Фев', 'Мар', 'Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек']
+  months: string[] = ['', 'Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
   selectedLegendID: string
 
   constructor(
@@ -30,10 +30,10 @@ export class CalendarComponent implements OnInit {
     this.refresh()
   }
 
-  refresh(){
+  refresh() {
     this.route.paramMap.subscribe(
       (params: ParamMap) => {
-        if (params.has('id')){
+        if (params.has('id')) {
           this.isPending = true
           this.calendarId = params.get('id')
           this.calendarService.getCalendarById(this.calendarId)
@@ -48,20 +48,22 @@ export class CalendarComponent implements OnInit {
     )
   }
 
-  selectLegend(id: string){
+  selectLegend(id: string) {
     this.clear = false
     this.selectedLegendID = id
   }
 
-  addDay(day: number, month: string){
-    const monthIndex = this.months.indexOf(month)
-    if (this.clear && !this.selectedLegendID){
-      const dayToDelete = this.calendar.days.find(d => d.day === day && d.month === monthIndex)
-      if (dayToDelete){
-        this.calendarService.deleteDay(this.calendarId, dayToDelete._id)
+  addDay(day: number, month: string) {
+    const validationResult = this.validDays(day, month)
+    if (validationResult) {
+      const monthIndex = this.months.indexOf(month)
+      if (this.clear && !this.selectedLegendID) {
+        this.deleteDay(day, month, monthIndex)
+      } else {
+        this.calendarService.addDayToCalendar(day, monthIndex, this.selectedLegendID, this.calendarId)
           .subscribe(
-            (data) => {
-              MaterializeService.toast(data.msg)
+            (calendar: Calendar) => {
+              MaterializeService.toast('Day added')
               this.refresh()
             },
             error => {
@@ -70,10 +72,43 @@ export class CalendarComponent implements OnInit {
           )
       }
     } else {
-      this.calendarService.addDayToCalendar(day, monthIndex, this.selectedLegendID, this.calendarId)
+      MaterializeService.toast('Такого дня в календаре не существует')
+    }
+
+  }
+
+  setLegend(day: number, month: string): Day | undefined {
+    const monthIndex = this.months.indexOf(month)
+    const dayToColor = this.calendar.days.find(p => p.day === day && p.month === monthIndex)
+    return dayToColor
+  }
+
+  selectClear() {
+    this.selectedLegendID = null
+    this.clear = true
+  }
+
+  private validDays(day: number, month: string) {
+    const invalidDays = [
+      {day: 30, month: 'Фев'}, {day: 31, month: 'Фев'}, {day: 31, month: 'Апр'},
+      {day: 31, month: 'Июн'}, {day: 31, month: 'Сен'}, {day: 31, month: 'Ноя'},
+    ]
+
+    const result = invalidDays.find(obj => obj.day === day && obj.month === month)
+    if (result) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  private deleteDay(day: number, month: string, monthIndex: number){
+    const dayToDelete = this.calendar.days.find(d => d.day === day && d.month === monthIndex)
+    if (dayToDelete) {
+      this.calendarService.deleteDay(this.calendarId, dayToDelete._id)
         .subscribe(
-          (calendar: Calendar) => {
-            MaterializeService.toast('Day added')
+          (data) => {
+            MaterializeService.toast(data.msg)
             this.refresh()
           },
           error => {
@@ -81,18 +116,6 @@ export class CalendarComponent implements OnInit {
           }
         )
     }
-
-  }
-
-  setLegend(day: number, month: string): Day | undefined{
-    const monthIndex = this.months.indexOf(month)
-    const dayToColor = this.calendar.days.find(p => p.day === day && p.month === monthIndex)
-    return dayToColor
-  }
-
-  selectClear(){
-    this.selectedLegendID = null
-    this.clear = true
   }
 
 }
